a99da8783a36105351e18d08fb17957a248f174a
humeman <camdenjsf@gmail.com>
2021-07-11 20:56:35 -0500

    Reraise instead of sending new SysExit

--- classes/bot.py
+++ classes/bot.py
@@ -270,7 +270,7 @@ def on_error_ext(*args, **kwargs):
 def catch_asyncio(loop, context):
     if "exception" in context:
         if type(context["exception"] in [SystemExit, KeyboardInterrupt]):
-            raise SystemExit
+            raise context["exception"] # Reraise
 
     # Just log it
     logger.log(
